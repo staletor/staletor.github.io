a20b7153aaba2086de635d0dabd39393a4788e38
staletor <st.stokke@gmail.com>
2015-03-31 13:00:32 +0200

    how about now

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 var customScripts = {
     profile: function () {
         // portfolio
-/* 		$('.isotopeWrapper').isotope({ filter: "*" }); */
+		$('.isotopeWrapper').isotope({ filter: '*' });
         if ($('.isotopeWrapper').length) {
 		
             var $container = $('.isotopeWrapper');
