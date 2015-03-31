223e3801dd628587d188aac3ded775307e582162
staletor <st.stokke@gmail.com>
2015-03-31 13:11:07 +0200

    Adjust script.js

--- js/script.js
+++ js/script.js
@@ -1,75 +1,18 @@
-/* $(document).ready(function() {
-
-			$("#aboutUs").waypoint(function(event, direction) {
-			   if (direction === 'down') {
-				  $("#header").animate({backgroundColor: 'red'},1000)
-			   }
-			   else {
-				  $("#header").animate({backgroundColor: 'blue'},1000)
-			   }
-			});
-
-			$("#skills").waypoint(function(event, direction) {
-			   if (direction === 'down') {
-				  $("#header").animate({backgroundColor: 'green'},1000)
-			   }
-			   else {
-				  $("#header").animate({backgroundColor: 'red'},1000)
-			   }
-			});
-
-			$("#experience").waypoint(function(event, direction) {
-			   if (direction === 'down') {
-				  $("#header").animate({backgroundColor: 'yellow'},1000)
-			   }
-			   else {
-				  $("#header").animate({backgroundColor: 'green'},1000)
-			   }
-			});
-
-			$("#portfolio").waypoint(function(event, direction) {
-			   if (direction === 'down') {
-				  $("#header").animate({backgroundColor: 'orange'},1000)
-			   }
-			   else {
-				  $("#header").animate({backgroundColor: 'yellow'},1000)
-			   }
-			});
-
-			
-		});
-		 */
-/* 		
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 210) {
-                    $("#header").css('background-color', 'blue');
-                } else {
-                    $("#header").css('background-color', 'red');
-                }
-            });
-        }); */
-		
 $(document).ready(function(){
 
-		$("#header").css({
-					backgroundColor: '#555'
-				});
 		
 		$('#home').waypoint(function() {
 
 				$("#header").css({
-					backgroundColor: '#555'
+					backgroundColor: '#555555'
 				});
 
-			}, {offset: '-1px'});
+			}, {offset: '1px'});
 			
 		$('#home').waypoint(function() {
 
 				$("#header").css({
-					backgroundColor: '#555'
+					backgroundColor: '#555555'
 				});
 
 			}, {offset: '-200px'});
