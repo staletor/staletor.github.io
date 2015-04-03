a6a1d4827ab9ab78097b3aed41a4011da11c53cf
staletor <st.stokke@gmail.com>
2015-04-03 13:15:27 +0200

    Many adjustments

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
 				$("#header").css({
 					backgroundColor: '#555555'
 				});
-
+				$("#topHome").css({
+					backgroundColor: '#555555'
+				});
 			}, {offset: '1px'});
 			
 		$('#home').waypoint(function() {
@@ -14,12 +16,17 @@ $(document).ready(function(){
 				$("#header").css({
 					backgroundColor: '#555555'
 				});
-
+				$("#topHome").css({
+					backgroundColor: '#555555'
+				});
 			}, {offset: '-200px'});
 		
 		$('#aboutUs').waypoint(function() {
 
 				$("#header").css({
+					backgroundColor: '#0f6b6b'
+				});
+				$("#topHome").css({
 					backgroundColor: '#0f6b6b'
 				});
 
@@ -28,6 +35,9 @@ $(document).ready(function(){
 		$('#aboutUs').waypoint(function() {
 
 				$("#header").css({
+					backgroundColor: '#0f6b6b'
+				});
+				$("#topHome").css({
 					backgroundColor: '#0f6b6b'
 				});
 
@@ -38,12 +48,18 @@ $(document).ready(function(){
 				$("#header").css({
 					backgroundColor: '#C4509F'
 				});
+				$("#topHome").css({
+					backgroundColor: '#C4509F'
+				});
 
 			}, { offset: '100px' });
 			
 		$('#skills').waypoint(function() {
 
 				$("#header").css({
+					backgroundColor: '#C4509F'
+				});
+				$("#topHome").css({
 					backgroundColor: '#C4509F'
 				});
 
@@ -54,12 +70,18 @@ $(document).ready(function(){
 				$("#header").css({
 					backgroundColor: '#5b846b'
 				});
+				$("#topHome").css({
+					backgroundColor: '#5b846b'
+				});
 
 			}, { offset: '100px' });
 			
 		$('#experience').waypoint(function() {
 
 				$("#header").css({
+					backgroundColor: '#5b846b'
+				});
+				$("#topHome").css({
 					backgroundColor: '#5b846b'
 				});
 
@@ -70,12 +92,18 @@ $(document).ready(function(){
 				$("#header").css({
 					backgroundColor: '#4d6159'
 				});
+				$("#topHome").css({
+					backgroundColor: '#4d6159'
+				});
 
 			}, { offset: '100px' });
 			
 		$('#portfolio').waypoint(function() {
 
 				$("#header").css({
+					backgroundColor: '#4d6159'
+				});
+				$("#topHome").css({
 					backgroundColor: '#4d6159'
 				});
 
@@ -84,6 +112,9 @@ $(document).ready(function(){
 		$('#contactUs').waypoint(function() {
 
 				$("#header").css({
+					backgroundColor: '#BCB382'
+				});
+				$("#topHome").css({
 					backgroundColor: '#BCB382'
 				});
 
